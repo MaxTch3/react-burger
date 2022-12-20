1ef5625d124bd7a43edce4f6a0fc7ea38243ed5b
Maxim Chalikov <aracctur@yandex.ru>
2022-12-20 13:41:39 +0300

    fix: AppHeader

--- src/components/app-header/app-header.jsx
+++ src/components/app-header/app-header.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
 import { Logo, BurgerIcon, ListIcon, ProfileIcon, } from '@ya.praktikum/react-developer-burger-ui-components';
 import style from './app-header.module.css'
+
 const AppHeader = () => {
    return (
       <header className={"pt-4 pb-4 " + style.header}>
@@ -12,19 +12,18 @@ const AppHeader = () => {
                </div>
                <div className={"pt-4 pr-5 pb-4 pl-5 ml-2 " + style.header__link}>
                   <ListIcon type="secondary" />
-                  <span className="text text_type_main-default ml-2">Лента заказов</span>
+                  <span style={{color: '#8585AD'}} className="text text_type_main-default ml-2">Лента заказов</span>
                </div>
             </div>
             <div className={style.header__logo}>
                <Logo />
             </div>
 
-            <div className={"pt-4 pr-5 pb-4 pl-5 " + style.header__link}>
+            <div style={{color: '#8585AD'}} className={"pt-4 pr-5 pb-4 pl-5 " + style.header__link}>
                <ProfileIcon type="secondary" />
                <span className="text text_type_main-default ml-2">Личный кабинет</span>
             </div>
          </div>
-
       </header>
    )
 }
