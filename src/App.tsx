7130c9fc4fff4785ef8af9a1c9b5f44e94f8b515
Maxim Chalikov <aracctur@yandex.ru>
2023-01-06 16:36:29 +0300

    feat: наброски на элемент конструктора

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import AppHeader from './components/app-header/app-header'
 import BurgerIngredients from './components/burger-ingredients/burger-ingredients.jsx'
+import BurgerConstructor from './components/burger-constructor/burger-constructor.jsx'
 import './App.css';
 import data from './utils/data.js';
 
@@ -13,12 +14,13 @@ function App() {
          justifyContent: 'center',
          maxWidth: '1280px',
          width: '100%',
-         marginLeft:'auto',
-         marginRight:'auto'
+         marginLeft: 'auto',
+         marginRight: 'auto'
       }}>
          <AppHeader />
          <main>
             <BurgerIngredients data={data} />
+            <BurgerConstructor />
          </main>
 
 
---
+++ src/components/burger-constructor/burger-constructor.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { 
+   CurrencyIcon, 
+   LockIcon, 
+   DeleteIcon, 
+   DragIcon, 
+   ConstructorElement, 
+   Button} from '@ya.praktikum/react-developer-burger-ui-components';
+
+const BurgerConstructor = () => {
+   return (
+      <div>Привет</div>
+   )
+}
+
+export default BurgerConstructor
