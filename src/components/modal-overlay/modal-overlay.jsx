eda5b6be1d14f799dd82047794ad8298fce88069
Maxim Chalikov <aracctur@yandex.ru>
2023-01-13 15:36:13 +0300

    fix: мелкие правки

--- src/components/modal-overlay/modal-overlay.jsx
+++ src/components/modal-overlay/modal-overlay.jsx
@@ -1,3 +1,12 @@
-import React from 'react';
-import styles from './modal-overlay.module.css';
+import styles from './modal-overlay.module.css'
+
+const ModalOverlay = ({ active, setActive, children }) => {
+  return (
+    <div className={active ? `${styles.modal_overlay} ${styles.modal_overlay_active}` : `${styles.modal_overlay}`} onClick={() => setActive(false)}>
+      {children}
+    </div>
+  )
+}
+
+export default ModalOverlay;
 
--- src/components/modal/modal.jsx
+++ src/components/modal/modal.jsx
@@ -1,15 +1,16 @@
 import styles from './modal.module.css';
+import ModalOverlay from '../modal-overlay/modal-overlay.jsx';
 
 const Modal = ({ active, setActive, children }) => {
 
   return (
-    <div className={active ? `${styles.modal} ${styles.modal_active}` : `${styles.modal}`} onClick={() => setActive(false)}>
+    <ModalOverlay active={active} setActive={setActive}>
       <div
         className={active ? `${styles.modal__content} ${styles.modal__content_active}` : `${styles.modal__content}`}
         onClick={(evt) => evt.stopPropagation()}>
         {children}
       </div>
-    </div>
+    </ ModalOverlay>
   )
 }
 export default Modal;
