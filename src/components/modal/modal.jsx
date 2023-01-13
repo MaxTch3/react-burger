432668bf05b2426f75659b404a7728697ec04d75
Maxim Chalikov <aracctur@yandex.ru>
2023-01-13 16:14:08 +0300

    добавил закрытие модальных окон по Esc

--- src/components/modal/modal.jsx
+++ src/components/modal/modal.jsx
@@ -1,7 +1,17 @@
+import React from 'react';
 import styles from './modal.module.css';
 import ModalOverlay from '../modal-overlay/modal-overlay.jsx';
 
 const Modal = ({ active, setActive, children }) => {
+  React.useEffect(() => {
+    const close = (evt) => {
+      if (evt.key === 'Escape') {
+        setActive(false)
+      }
+    }
+    document.addEventListener('keydown', close)
+    return () => document.removeEventListener('keydown', close)
+  }, [])
 
   return (
     <ModalOverlay active={active} setActive={setActive}>
