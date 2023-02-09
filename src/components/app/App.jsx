edeb18d41fb02b111dc2f5efcafb32b8d4c60288
Maxim Chalikov <aracctur@yandex.ru>
2023-02-09 05:28:01 +0300

    feat: обернуть компоненты, участвующие в dnd в провайдер

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
 import AppHeader from '../app-header/app-header'
 import BurgerIngredients from '../burger-ingredients/burger-ingredients.jsx'
 import BurgerConstructor from '../burger-constructor/burger-constructor.jsx'
 import styles from './App.module.css';
-import { useDispatch } from 'react-redux';
 import { getIngredientsData } from '../../services/actions/ingredientsData.js';
 
 function App() {
-  //const [dataModal, setDataModal] = React.useState('');
   const dispatch = useDispatch();
 
   React.useEffect(() => {
@@ -18,11 +19,10 @@ function App() {
     <div className={styles.app}>
       <AppHeader />
       <main className={styles.main} >
-        <BurgerIngredients
-          // setDataModal={setDataModal}
-          // dataModal={dataModal}
-        />
-        <BurgerConstructor />
+        <DndProvider backend={HTML5Backend}>
+          <BurgerIngredients />
+          <BurgerConstructor />
+        </DndProvider>
       </main>
     </div>
   );
