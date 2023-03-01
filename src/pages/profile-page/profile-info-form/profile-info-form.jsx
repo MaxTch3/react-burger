dfbd0a4526d16435ba3839a39b10fe318c742955
Maxim Chalikov <aracctur@yandex.ru>
2023-03-01 22:23:56 +0300

    refact: выделить форму редактирования профиля из ProfilePage в отдельный компонент

---
+++ src/pages/profile-page/profile-info-form/profile-info-form.jsx
@@ -0,0 +1,99 @@
+import { Button, Input } from '@ya.praktikum/react-developer-burger-ui-components';
+import { useEffect, useRef, useState } from 'react';
+import styles from './profile-info-form.module.css';
+
+const ProfileInfoForm = () => {
+
+  const initialForm = { name: '', email: '', password: '' };
+  const initialIcons = { name: 'EditIcon', email: 'EditIcon', password: 'EditIcon' };
+  const [profileInfo, setProfileInfo] = useState(initialForm);
+  const [icon, setIcon] = useState(initialIcons);
+  const [activeButtons, setActiveButtons] = useState(false);
+  const nameRef = useRef();
+  const emailRef = useRef();
+  const passwordRef = useRef();
+
+  const onChangeInput = (e) => {
+    setProfileInfo({ ...profileInfo, [e.target.name]: e.target.value });
+    setIcon({ ...icon, [e.target.name]: 'CloseIcon' });
+    setActiveButtons(true)
+  };
+
+  const clearInput = (ref) => {
+    setProfileInfo({ ...profileInfo, [ref.current.name]: '' });
+    setIcon({ ...icon, [ref.current.name]: 'EditIcon' })
+  };
+
+  const resetForm = () => {
+    setProfileInfo(initialForm);
+    setIcon(initialIcons)
+  }
+
+  useEffect(() => {
+    if (JSON.stringify(profileInfo) === JSON.stringify(initialForm)) {
+      setActiveButtons(false)
+    }
+  }, [profileInfo])
+
+  return (
+    <form className={styles.form}>
+      <Input
+        type={'text'}
+        placeholder={'Имя'}
+        ref={nameRef}
+        onChange={onChangeInput}
+        value={profileInfo.name}
+        onIconClick={() => { clearInput(nameRef) }}
+        name={'name'}
+        error={false}
+        errorText={'Ошибка'}
+        size={'default'}
+        extraClass="ml-1 p"
+        icon={icon.name}
+      />
+      <Input
+        type={'email'}
+        placeholder={'Логин'}
+        ref={emailRef}
+        onChange={onChangeInput}
+        value={profileInfo.email}
+        onIconClick={() => { clearInput(emailRef) }}
+        name='email'
+        error={false}
+        errorText={'Ошибка'}
+        size={'default'}
+        extraClass="ml-1 p"
+        icon={icon.email}
+      />
+      <Input
+        type={'password'}
+        placeholder={'Пароль'}
+        ref={passwordRef}
+        onChange={onChangeInput}
+        value={profileInfo.password}
+        onIconClick={() => { clearInput(passwordRef) }}
+        name={'password'}
+        error={false}
+        errorText={'Ошибка'}
+        size={'default'}
+        extraClass="ml-1"
+        icon={icon.password}
+      />
+      {activeButtons &&
+        <div style={{ display: 'flex' }}>
+          <Button htmlType="button" type="secondary" size="medium"
+            onClick={resetForm}>
+            Отмена
+          </Button>
+          <Button
+            htmlType="button"
+            type="primary"
+            size="medium">
+            Сохранить
+          </Button>
+        </div>
+      }
+    </form>
+  )
+}
+export default ProfileInfoForm;
--- src/pages/profile-page/profile-page.jsx
+++ src/pages/profile-page/profile-page.jsx
@@ -1,42 +1,12 @@
-import { Button, Input } from '@ya.praktikum/react-developer-burger-ui-components';
-import { useEffect, useRef, useState } from 'react';
+import { useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
 import styles from './profile-page.module.css';
+import ProfileInfoForm from './profile-info-form/profile-info-form.jsx';
 
 const ProfilePage = () => {
   const on = '';
   const off = ' text_color_inactive';
-  const initialForm = { name: '', email: '', password: '' };
-  const initialIcons = { name: 'EditIcon', email: 'EditIcon', password: 'EditIcon' };
-
   const [linkStatus, setLinkStatus] = useState({ profileLink: on, orderListLink: off, logOutLink: off });
-  const [profileInfo, setProfileInfo] = useState(initialForm);
-  const [icon, setIcon] = useState(initialIcons);
-  const [activeButtons, setActiveButtons] = useState(false);
-  const nameRef = useRef();
-  const emailRef = useRef();
-  const passwordRef = useRef();
-
-  const onChangeInput = (e) => {
-    setProfileInfo({ ...profileInfo, [e.target.name]: e.target.value });
-    setIcon({ ...icon, [e.target.name]: 'CloseIcon' });
-    setActiveButtons(true)
-  };
-
-  const clearInput = (ref) => {
-    setProfileInfo({ ...profileInfo, [ref.current.name]: '' });
-    setIcon({ ...icon, [ref.current.name]: 'EditIcon' })
-  };
-
-  const resetForm = () => {
-    setProfileInfo(initialForm);
-    setIcon(initialIcons)
-  }
-
-  useEffect(() => {
-    if (JSON.stringify(profileInfo) === JSON.stringify(initialForm)) {
-      setActiveButtons(false)
-    }
-  }, [profileInfo])
 
   return (
     <div className={styles.container}>
@@ -58,65 +28,9 @@ const ProfilePage = () => {
         <p className="text text_type_main-default text_color_inactive pt-20" style={{ opacity: 0.4 }}>В этом разделе вы можете
           изменить&nbsp;свои персональные данные</p>
       </div>
-
-          <form className={styles.form}>
-            <Input
-              type={'text'}
-              placeholder={'Имя'}
-              ref={nameRef}
-              onChange={onChangeInput}
-              value={profileInfo.name}
-              onIconClick={() => { clearInput(nameRef) }}
-              name={'name'}
-              error={false}
-              errorText={'Ошибка'}
-              size={'default'}
-              extraClass="ml-1 p"
-              icon={icon.name}
-            />
-            <Input
-              type={'email'}
-              placeholder={'Логин'}
-              ref={emailRef}
-              onChange={onChangeInput}
-              value={profileInfo.email}
-              onIconClick={() => { clearInput(emailRef) }}
-              name='email'
-              error={false}
-              errorText={'Ошибка'}
-              size={'default'}
-              extraClass="ml-1 p"
-              icon={icon.email}
-            />
-            <Input
-              type={'password'}
-              placeholder={'Пароль'}
-              ref={passwordRef}
-              onChange={onChangeInput}
-              value={profileInfo.password}
-              onIconClick={() => { clearInput(passwordRef) }}
-              name={'password'}
-              error={false}
-              errorText={'Ошибка'}
-              size={'default'}
-              extraClass="ml-1"
-              icon={icon.password}
-            />
-            {activeButtons &&
-              <div style={{ display: 'flex' }}>
-                <Button htmlType="button" type="secondary" size="medium"
-                  onClick={resetForm}>
-                  Отмена
-                </Button>
-                <Button
-                  htmlType="button"
-                  type="primary"
-                  size="medium">
-                  Сохранить
-                </Button>
-              </div>
-            }
-          </form>
+      <Routes>
+        <Route path='/' element={<ProfileInfoForm />} />
+      </Routes>
     </div >
   )
 }
