e7b130b2e4d3381c524b6c1cf80cd0bfdc6eb687
Maxim Chalikov <aracctur@yandex.ru>
2023-03-01 17:08:46 +0300

    feat: поправлена функциональность ProfilePage

--- src/pages/profile-page/profile-page.jsx
+++ src/pages/profile-page/profile-page.jsx
@@ -1,14 +1,17 @@
 import { Button, Input } from '@ya.praktikum/react-developer-burger-ui-components';
 import { useEffect, useRef, useState } from 'react';
-import styles from './profile-page.module.css'
+import styles from './profile-page.module.css';
 
 const ProfilePage = () => {
   const on = '';
   const off = ' text_color_inactive';
+  const initialForm = { name: '', email: '', password: '' };
+  const initialIcons = { name: 'EditIcon', email: 'EditIcon', password: 'EditIcon' };
+
   const [linkStatus, setLinkStatus] = useState({ profileLink: on, orderListLink: off, logOutLink: off });
-  const [profileInfo, setProfileInfo] = useState({ name: '', email: '', password: '' });
-  const [icon, setIcon] = useState({ name: 'EditIcon', email: 'EditIcon', password: 'EditIcon' });
-  const [activeButtons, setActiveButtons] = useState(false)
+  const [profileInfo, setProfileInfo] = useState(initialForm);
+  const [icon, setIcon] = useState(initialIcons);
+  const [activeButtons, setActiveButtons] = useState(false);
   const nameRef = useRef();
   const emailRef = useRef();
   const passwordRef = useRef();
@@ -17,14 +20,20 @@ const ProfilePage = () => {
     setProfileInfo({ ...profileInfo, [e.target.name]: e.target.value });
     setIcon({ ...icon, [e.target.name]: 'CloseIcon' });
     setActiveButtons(true)
-  }
+  };
 
   const clearInput = (ref) => {
     setProfileInfo({ ...profileInfo, [ref.current.name]: '' });
     setIcon({ ...icon, [ref.current.name]: 'EditIcon' })
+  };
+
+  const resetForm = () => {
+    setProfileInfo(initialForm);
+    setIcon(initialIcons)
   }
+
   useEffect(() => {
-    if (!profileInfo.name && !profileInfo.email && !profileInfo.password) {
+    if (JSON.stringify(profileInfo) === JSON.stringify(initialForm)) {
       setActiveButtons(false)
     }
   }, [profileInfo])
@@ -49,64 +58,66 @@ const ProfilePage = () => {
         <p className="text text_type_main-default text_color_inactive pt-20" style={{ opacity: 0.4 }}>В этом разделе вы можете
           изменить&nbsp;свои персональные данные</p>
       </div>
-      <form className={styles.form}>
-        <Input
-          type={'text'}
-          placeholder={'Имя'}
-          ref={nameRef}
-          onChange={onChangeInput}
-          value={profileInfo.name}
-          onIconClick={() => { clearInput(nameRef) }}
-          name={'name'}
-          error={false}
-          errorText={'Ошибка'}
-          size={'default'}
-          extraClass="ml-1 p"
-          icon={icon.name}
-        />
-        <Input
-          type={'email'}
-          placeholder={'Логин'}
-          ref={emailRef}
-          onChange={onChangeInput}
-          value={profileInfo.email}
-          onIconClick={() => { clearInput(emailRef) }}
-          name='email'
-          error={false}
-          errorText={'Ошибка'}
-          size={'default'}
-          extraClass="ml-1 p"
-          icon={icon.email}
-        />
-        <Input
-          type={'password'}
-          placeholder={'Пароль'}
-          ref={passwordRef}
-          onChange={onChangeInput}
-          value={profileInfo.password}
-          onIconClick={() => { clearInput(passwordRef) }}
-          name={'password'}
-          error={false}
-          errorText={'Ошибка'}
-          size={'default'}
-          extraClass="ml-1"
-          icon={icon.password}
-        />
-        {activeButtons &&
-          <div style={{ display: 'flex' }}>
-            <Button htmlType="button" type="secondary" size="medium">
-              Отмена
-            </Button>
-            <Button
-              htmlType="button"
-              type="primary"
-              size="medium">
-              Сохранить
-            </Button>
-          </div>
-        }
-      </form>
-    </div>
+
+          <form className={styles.form}>
+            <Input
+              type={'text'}
+              placeholder={'Имя'}
+              ref={nameRef}
+              onChange={onChangeInput}
+              value={profileInfo.name}
+              onIconClick={() => { clearInput(nameRef) }}
+              name={'name'}
+              error={false}
+              errorText={'Ошибка'}
+              size={'default'}
+              extraClass="ml-1 p"
+              icon={icon.name}
+            />
+            <Input
+              type={'email'}
+              placeholder={'Логин'}
+              ref={emailRef}
+              onChange={onChangeInput}
+              value={profileInfo.email}
+              onIconClick={() => { clearInput(emailRef) }}
+              name='email'
+              error={false}
+              errorText={'Ошибка'}
+              size={'default'}
+              extraClass="ml-1 p"
+              icon={icon.email}
+            />
+            <Input
+              type={'password'}
+              placeholder={'Пароль'}
+              ref={passwordRef}
+              onChange={onChangeInput}
+              value={profileInfo.password}
+              onIconClick={() => { clearInput(passwordRef) }}
+              name={'password'}
+              error={false}
+              errorText={'Ошибка'}
+              size={'default'}
+              extraClass="ml-1"
+              icon={icon.password}
+            />
+            {activeButtons &&
+              <div style={{ display: 'flex' }}>
+                <Button htmlType="button" type="secondary" size="medium"
+                  onClick={resetForm}>
+                  Отмена
+                </Button>
+                <Button
+                  htmlType="button"
+                  type="primary"
+                  size="medium">
+                  Сохранить
+                </Button>
+              </div>
+            }
+          </form>
+    </div >
   )
 }
 
