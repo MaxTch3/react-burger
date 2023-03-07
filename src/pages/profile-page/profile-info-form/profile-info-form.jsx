9091c6346d8cb921cda87c9e71f658000f3b2c05
Maxim Chalikov <aracctur@yandex.ru>
2023-03-07 11:28:07 +0300

    fix: изменить логику поведения элементов в форме профиля, кнопки появляются лишь тогда, когда есть изменения в полях имени или почты и поле пароля не пустое, кнопки редактирования открывают поле для изменения, кнопка закрытия сбрасывает к начальному состоянию

--- src/pages/profile-page/profile-info-form/profile-info-form.jsx
+++ src/pages/profile-page/profile-info-form/profile-info-form.jsx
@@ -7,39 +7,60 @@ const ProfileInfoForm = () => {
 
   const initialForm = { name: '', email: '', password: '' };
   const initialIcons = { name: 'EditIcon', email: 'EditIcon', password: 'EditIcon' };
+  const initialDisabled = { name: true, email: true, password: true };
+
   const [profileInfo, setProfileInfo] = useState(initialForm);
   const [icon, setIcon] = useState(initialIcons);
+  const [isDisabled, setIsDisabled] = useState(initialDisabled)
   const [activeButtons, setActiveButtons] = useState(false);
+
   const nameRef = useRef();
   const emailRef = useRef();
   const passwordRef = useRef();
-  const userInfo = useSelector(state => state.userReducer.user)
+
+  const userInfo = useSelector(state => state.userReducer.user);
 
   const onChangeInput = (e) => {
     setProfileInfo({ ...profileInfo, [e.target.name]: e.target.value });
-    setIcon({ ...icon, [e.target.name]: 'CloseIcon' });
     setActiveButtons(true)
   };
 
-  const clearInput = (ref) => {
-    setProfileInfo({ ...profileInfo, [ref.current.name]: '' });
-    setIcon({ ...icon, [ref.current.name]: 'EditIcon' })
+  const clickIcon = (ref) => {
+    if (isDisabled[ref.current.name]) {
+      setIsDisabled({ ...isDisabled, [ref.current.name]: false });
+      setIcon({ ...icon, [ref.current.name]: 'CloseIcon' })
+    } else {
+      setIsDisabled({ ...isDisabled, [ref.current.name]: true });
+      setIcon({ ...icon, [ref.current.name]: 'EditIcon' });
+      const resetElement = () => {
+        if (ref.current.name !== 'password') { return userInfo[ref.current.name] }
+        else { return '' }
+      }
+      setProfileInfo({ ...profileInfo, [ref.current.name]: resetElement() })
+    }
   };
 
   const resetForm = () => {
     setProfileInfo({ name: userInfo.name, email: userInfo.email, password: '' })
-    setIcon(initialIcons)
+    setIcon(initialIcons);
+    setIsDisabled(initialDisabled)
   }
 
   useEffect(() => {
-    if (JSON.stringify(profileInfo) === JSON.stringify({ name: userInfo.name, email: userInfo.email, password: '' })) {
+    if (
+      ((profileInfo.name === userInfo.name) && (profileInfo.email === userInfo.email))
+      || !profileInfo.name
+      || !profileInfo.email
+      || !profileInfo.password) {
       setActiveButtons(false)
     }
   }, [profileInfo])
 
   useEffect(() => {
     setProfileInfo({ password: '', name: userInfo.name, email: userInfo.email })
-  }, [])
+  }, []);
+
+
 
   return (
     <form className={styles.form}>
@@ -49,13 +70,14 @@ const ProfileInfoForm = () => {
         ref={nameRef}
         onChange={onChangeInput}
         value={profileInfo.name}
-        onIconClick={() => { clearInput(nameRef) }}
+        onIconClick={() => { clickIcon(nameRef) }}
         name={'name'}
         error={false}
         errorText={'Ошибка'}
         size={'default'}
         extraClass="ml-1 p"
         icon={icon.name}
+        disabled={isDisabled.name}
       />
       <Input
         type={'email'}
@@ -63,13 +85,14 @@ const ProfileInfoForm = () => {
         ref={emailRef}
         onChange={onChangeInput}
         value={profileInfo.email}
-        onIconClick={() => { clearInput(emailRef) }}
+        onIconClick={() => { clickIcon(emailRef) }}
         name='email'
         error={false}
         errorText={'Ошибка'}
         size={'default'}
         extraClass="ml-1 p"
         icon={icon.email}
+        disabled={isDisabled.email}
       />
       <Input
         type={'password'}
@@ -77,13 +100,14 @@ const ProfileInfoForm = () => {
         ref={passwordRef}
         onChange={onChangeInput}
         value={profileInfo.password}
-        onIconClick={() => { clearInput(passwordRef) }}
+        onIconClick={() => { clickIcon(passwordRef) }}
         name={'password'}
         error={false}
         errorText={'Ошибка'}
         size={'default'}
         extraClass="ml-1"
         icon={icon.password}
+        disabled={isDisabled.password}
       />
       {activeButtons &&
         <div style={{ display: 'flex' }}>
