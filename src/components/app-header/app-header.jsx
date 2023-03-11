562e74e9441ace2f59ebdd47ff508748e95e7d9f
Maxim Chalikov <aracctur@yandex.ru>
2023-03-11 16:35:56 +0300

    fix: исправлена ошибка с активностью ссылок в шапке

--- src/components/app-header/app-header.jsx
+++ src/components/app-header/app-header.jsx
@@ -1,18 +1,12 @@
 import { Logo, BurgerIcon, ListIcon, ProfileIcon, } from '@ya.praktikum/react-developer-burger-ui-components';
-import { Link, useLocation } from 'react-router-dom';
+import { Link, useMatch } from 'react-router-dom';
 import styles from './app-header.module.css'
 
 const AppHeader = () => {
-  const location = useLocation();
 
-  const iconStatus = (path) => {
-    if (location.pathname === path) { return 'primary' }
-    else { return 'secondary' }
-  }
-  const textStyle = (path) => {
-    if (location.pathname === path) { return '' }
-    else { return 'text_color_inactive' }
-  }
+  const constructorActive = useMatch('/');
+  const feedActive = useMatch('/feed');
+  const profileActive = useMatch('/profile/*');
 
   return (
     <header className={'pt-4 pb-4 ' + styles.header}>
@@ -21,15 +15,15 @@ const AppHeader = () => {
           <Link className={'pt-4 pr-5 pb-4 pl-5 ' + styles.header__link}
             to='/'
           >
-            <BurgerIcon type={iconStatus('/')} />
-            <span className={`text text_type_main-default ml-2 ${textStyle('/')}`}
+            <BurgerIcon type={constructorActive ? 'primary' : 'secondary'} />
+            <span className={`text text_type_main-default ml-2 ${constructorActive ? '' : 'text_color_inactive'}`}
             >Конструктор</span>
           </Link>
           <Link className={'pt-4 pr-5 pb-4 pl-5 ml-2 ' + styles.header__link}
             to='/feed'
           >
-            <ListIcon type={iconStatus('/feed')} />
-            <span className={`text text_type_main-default ml-2 ${textStyle('/feed')}`}>Лента заказов</span>
+            <ListIcon type={feedActive ? 'primary' : 'secondary'} />
+            <span className={`text text_type_main-default ml-2 ${feedActive ? '' : 'text_color_inactive'}`}>Лента заказов</span>
           </Link>
         </div>
         <div className={styles.header__logo}>
@@ -39,8 +33,8 @@ const AppHeader = () => {
         <Link className={'pt-4 pr-5 pb-4 pl-5 ' + styles.header__link}
           to='/profile'
         >
-          <ProfileIcon type={iconStatus('/profile')} />
-          <span className={`text text_type_main-default ml-2 ${textStyle('/profile')}`}>Личный кабинет</span>
+          <ProfileIcon type={profileActive ? 'primary' : 'secondary'} />
+          <span className={`text text_type_main-default ml-2 ${profileActive ? '' : 'text_color_inactive'}`}>Личный кабинет</span>
         </Link>
       </nav>
     </header>
