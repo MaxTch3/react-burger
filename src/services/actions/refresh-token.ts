79c14a76939bdb54914bc17aa5e146b43addff74
Maxim Chalikov <aracctur@yandex.ru>
2023-04-26 12:17:39 +0300

    refact: типизировать refresh-token

---
+++ src/services/actions/refresh-token.ts
@@ -0,0 +1,42 @@
+import { AppDispatch } from "../../components/app/App";
+import { setCookie } from "../../utils/cookie-functions";
+import { refreshTokenRequest } from "../../utils/user-api";
+
+export const REFRESH_TOKEN_REQUEST: 'REFRESH_TOKEN_REQUEST' = 'REFRESH_TOKEN_REQUEST';
+export const REFRESH_TOKEN_SUCCESS: 'REFRESH_TOKEN_SUCCESS' = 'REFRESH_TOKEN_SUCCESS';
+export const REFRESH_TOKEN_FAILED: 'REFRESH_TOKEN_FAILED' = 'REFRESH_TOKEN_FAILED';
+
+export interface IRefreshTokenRequest {
+  readonly type: typeof REFRESH_TOKEN_REQUEST;
+}
+export interface IRefreshTokenSuccess {
+  readonly type: typeof REFRESH_TOKEN_SUCCESS
+}
+export interface IRefreshTokenFailed {
+  readonly type: typeof REFRESH_TOKEN_FAILED;
+}
+
+export type IRefreshTokenActions =
+  IRefreshTokenRequest
+  | IRefreshTokenSuccess
+  | IRefreshTokenFailed
+
+const refreshTokenAction = () => (dispatch: AppDispatch) => {
+  dispatch({ type: REFRESH_TOKEN_REQUEST });
+  refreshTokenRequest()
+    .then(res => {
+      if (res && res.success) {
+        setCookie('token', res.accessToken, { expires: 1200 });
+        localStorage.setItem("jwt", res.refreshToken);
+        dispatch({ type: REFRESH_TOKEN_SUCCESS })
+      }
+      else {
+        dispatch({ type: REFRESH_TOKEN_FAILED })
+      }
+    })
+    .catch(() => {
+      dispatch({ type: REFRESH_TOKEN_FAILED })
+    })
+}
+
+export default refreshTokenAction;
